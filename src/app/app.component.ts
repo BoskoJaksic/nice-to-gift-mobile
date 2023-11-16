45bb5780419478722cd127233aa4ae9d7963133e
boskojaksic <bosko.jaksic@wedosoftware.eu>
2023-11-16 09:51:29 +0100

    well known added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, NgZone} from '@angular/core';
 import {register} from 'swiper/element/bundle';
 import {StatusBar, Style} from '@capacitor/status-bar';
 import { Stripe } from '@capacitor-community/stripe';
 import {environment} from "../environments/environment";
 import {LoaderService} from "./shared/services/loader.service";
+import {App, URLOpenListenerEvent} from "@capacitor/app";
+import { Router } from '@angular/router';
 
 register();
 @Component({
@@ -12,7 +14,8 @@ register();
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor( public loaderService: LoaderService,) {
+  constructor( public loaderService: LoaderService,private _ngZone: NgZone,private _router: Router) {
+    this.initApp();
     Stripe.initialize({
       publishableKey: environment.stripe.publishKey,
     });
@@ -24,5 +27,21 @@ export class AppComponent {
   }
   ngOnInit() {
 
+  }
+
+  initApp() {
+    App.addListener('appUrlOpen', (event: URLOpenListenerEvent) => {
+      this._ngZone.run(() => {
+        // Example url: https://my-ionic.app/tabs/tab2
+        // slug = /tabs/tab2
+        const slug = event.url.split(".app").pop();
+        console.log("slug = ", slug);
+        if (slug) {
+          this._router.navigateByUrl(slug);
+        }
+        // If no match, do nothing - let regular routing
+        // logic take over
+      });
+    });
   }
 }
